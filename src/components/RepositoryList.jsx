b9196d692932bdbb0d855a6a08b2a122bc1267d1
JoaoOliveira0117 <joao.oliveira0117@hotmail.com>
2021-08-25 20:27:11 -0300

    Adicionar request por url

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,43 +1,32 @@
-const repositoryName = 'unform2';
+import { useState, useEffect } from 'react';
+import { RepositoryItem } from './RepositoryItem';
+
+import '../styles/repositories.scss';
+
+
+const repository = {
+    name: 'unform',
+    description: 'Forms in React',
+    link: 'https://github.com/unform/unform',
+}
 
 export function RepositoryList(){
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(()=>{
+        fetch('https://api.github.com/users/JoaoOliveira0117/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data));
+    }, []);
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <li>
-                    <strong>{repositoryName}</strong>
-                    <p>Forms in ReactJS</p>
-
-                    <a href="">
-                        Acessar Repositorio
-                    </a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in ReactJS</p>
-
-                    <a href="">
-                        Acessar Repositorio
-                    </a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in ReactJS</p>
-
-                    <a href="">
-                        Acessar Repositorio
-                    </a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in ReactJS</p>
-
-                    <a href="">
-                        Acessar Repositorio
-                    </a>
-                </li>
+                {repositories.map(repository =>
+                    <RepositoryItem key={repository.name} repository={repository}/>
+                )}
             </ul>
         </section>
     );
